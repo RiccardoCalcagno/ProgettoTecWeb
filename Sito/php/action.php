c4a793e893586363ba3e91c124cacd3a7e0eb3e6
AlessandroPirolo <Alessandro.pirolo@studenti.unipd.it>
2021-01-02 17:54:35 +0100

    Aggiunto action.php

---
+++ Sito/php/action.php
@@ -0,0 +1,45 @@
+<?php
+
+if(isset($_POST["Accedi"]))
+{
+    if($_POST["Accedi"] == "Accedi")
+    {
+        header("Location : login.php");
+        exit();
+    }
+
+    if($_POST["Accedi"] == "Esci")
+    {
+        session_start();
+        
+        session_destroy();
+        
+        header('Location: index.php');
+        
+        die();
+    }
+
+}
+
+if(isset($_POST["Iscrizione"]))
+{
+    if($_POST["Iscrizione"] == "Iscrizione")
+    {
+        header("Location : register.php");
+        exit();
+    }
+
+    if($_POST["Iscrizione"] == "Area Personale")
+    {
+        header("Location : area_personale.php");
+        exit();
+    }
+}
+
+header("Location : index.php");
+
+
+
+
+
+?>
