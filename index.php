7f0711a59e49cd14402a51958c97ab2f25ed3beb
RiccardoCalcagno <63152853+RiccardoCalcagno@users.noreply.github.com>
2021-01-06 18:13:34 +0100

    debug 6

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("php" . DIRECTORY_SEPARATOR . "banners.php");
 
 $html = file_get_contents("html" . DIRECTORY_SEPARATOR . "Home.html");
 
+$html = str_replace("<body>","<body> <h1>OOOOOOOOOOOOOO</h1>",$html);
 $html = setup_clear($html);
 
 $html = addPossibleBanner($html, "index.php");
