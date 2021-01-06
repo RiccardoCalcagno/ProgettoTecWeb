a7afc280345169858cc3613ed9988fd31659cff3
RiccardoCalcagno <63152853+RiccardoCalcagno@users.noreply.github.com>
2021-01-06 23:15:15 +0100

    debug 14

--- php/EsploraPage.php
+++ php/EsploraPage.php
@@ -21,8 +21,8 @@ else{
     $html = file_get_contents('..'. DIRECTORY_SEPARATOR . 'html' . DIRECTORY_SEPARATOR . 'Esplora.html');
     $html = setup($html);
 
-    $_SESSION["username"]="Barb";
-    $_SESSION["passwd"]="12345678Aa";
+    $_SESSION["username"]="QueenAdministrator";
+    $_SESSION["passwd"]="1000BimbiFucsia";
 
     $_SESSION["vai_avanti_esplora"] = false;
     $_SESSION["vai_indietro_esplora"] = false;
