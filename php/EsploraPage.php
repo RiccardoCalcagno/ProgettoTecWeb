76098a7cb90fd5c2e2102f2b80d11607a06b9782
RiccardoCalcagno <63152853+RiccardoCalcagno@users.noreply.github.com>
2021-01-06 21:41:41 +0100

    debug 1

--- php/EsploraPage.php
+++ php/EsploraPage.php
@@ -11,8 +11,8 @@ $dbInterface = new DBinterface();
 $connection = $dbInterface->openConnection();
 
 if($connection == false){
-    //redirecting to 404
-    $dbInterface->closeConnection();
+    header("Location: Errore.php");
+    exit();
 }
 else{
 
@@ -46,7 +46,7 @@ else{
     $_schede_report_esplora = "";
 
     for($i = ($_SESSION["count_esplora"]-1)*5 ; $i < $limit = $_SESSION["num_report_esplora"] < $numero_pag_esplora ? $limit = $_SESSION["num_report_esplora"] : $limit = 5*$_SESSION["count_esplora"] ; $i++)
-    {
+        {
         $_schede_report_esplora .=   
         "<li class=\"cardReport\" class=\"cardReportEsplora\">
         <button name=\"ReportEsplora\" value=\"". $_SESSION["report_data"][$i]->get_id() . "\">
@@ -63,30 +63,30 @@ else{
             <p class=\"lableRepPublico\"><span xml:lang=\"en\">Report</span> condiviso a <span class=\"numCondivisioni\">" . $_SESSION["array_num_part_rep_esplora"][$_SESSION["report_data"][$i]->get_id()] . "</span> giocatori</p>
         </footer>
         </button>
-    </li>\n";
-    }  
+        </li>\n";
+        }  
         
-        if($_SESSION["count_esplora"] == 1)
+    if($_SESSION["count_esplora"] == 1)
         {
-            $html = str_replace("<li><label id=\"LblEsploraPrecedente\" for=\"esploraPrecedente\">precedente</label></li>
-            <li class=\"inputMove\"><input type=\"submit\" id=\"esploraPrecedente\" class=\"precedente\" name=\"espandi\" value=\"esploraPrecedente\"></li>", " ", $html);
+        $html = str_replace("<li><label id=\"LblEsploraPrecedente\" for=\"esploraPrecedente\">precedente</label></li>
+        <li class=\"inputMove\"><input type=\"submit\" id=\"esploraPrecedente\" class=\"precedente\" name=\"espandi\" value=\"esploraPrecedente\"></li>", " ", $html);
         }
 
-        if($_SESSION["count_esplora"] == $numero_pag_esplora)
+    if($_SESSION["count_esplora"] == $numero_pag_esplora)
         {
-            $html = str_replace("<li class=\"inputMove\"><input type=\"submit\" id=\"esploraSuccessivo\" class=\"successivo\" name=\"espandi\" value=\"esploraSuccessivo\"></li> 
-            <li><label id=\"LblEsploraSuccessivo\" for=\"esploraSuccessivo\">successiva</label></li>", " ", $html);
+        $html = str_replace("<li class=\"inputMove\"><input type=\"submit\" id=\"esploraSuccessivo\" class=\"successivo\" name=\"espandi\" value=\"esploraSuccessivo\"></li> 
+        <li><label id=\"LblEsploraSuccessivo\" for=\"esploraSuccessivo\">successiva</label></li>", " ", $html);
         }
 
-        if($_SESSION["count_esplora"] <= 5)
+    if($_SESSION["count_esplora"] <= 5)
         {
-            $html = str_replace("<nav class=\"espandi\">", "<nav class=\"espandi\" class=\"hidden\">", $html);
+        $html = str_replace("<nav class=\"espandi\">", "<nav class=\"espandi\" class=\"hidden\">", $html);
         }
 
 
-        $html = str_replace("<reports/>", $_schede_report_esplora, $html);
-        $html = str_replace("<currentPageReport/>", $_SESSION["count_esplora"], $html);
-        $html = str_replace("<totPagReport/>", $numero_pag_esplora, $html);
+    $html = str_replace("<reports/>", $_schede_report_esplora, $html);
+    $html = str_replace("<currentPageReport/>", $_SESSION["count_esplora"], $html);
+    $html = str_replace("<totPagReport/>", $numero_pag_esplora, $html);
 
 
 $html = addPossibleBanner($html, "EsploraPage.php");
