b8299a863ff2b7c28242208a708635e8f09af379
RiccardoCalcagno <63152853+RiccardoCalcagno@users.noreply.github.com>
2021-01-07 14:01:34 +0100

    d

--- php/DBinterface.php
+++ php/DBinterface.php
@@ -507,10 +507,10 @@
                      "FROM Report INNER JOIN Users ON Report.author = Users.username". 
                      "WHERE Report.author = '" . $username . "';";
             
-            $stringa="hey".mysqli_query(mysqli_connect( DBinterface::HOST, 
+            $stringa="hey".(mysqli_query(mysqli_connect( DBinterface::HOST, 
                             DBinterface::USERNAME, 
                             DBinterface::PASSWORD, 
-                            DBinterface::DB_NAME), $query);
+                            DBinterface::DB_NAME), $query)!=null);
 
             $query_result = mysqli_query($this->connection, $query);
 
