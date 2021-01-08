0f02e023e4d3e0596bafe501d751a8001189dfa0
RiccardoCalcagno <63152853+RiccardoCalcagno@users.noreply.github.com>
2021-01-08 02:30:56 +0100

    d

--- php/login.php
+++ php/login.php
@@ -10,7 +10,7 @@
     $username = ""; 
 
     $html = file_get_contents("..". DIRECTORY_SEPARATOR . "html". DIRECTORY_SEPARATOR . "login.html");
-    $html = setup($html);   //setup_clear() ?
+    $html = setup_clear($html);   //setup() ?
 
     if(isset($_SESSION['beforeAccess'])){
         $html = str_replace('<a href="../index.php" class="annulla">ANNULLA</a>',"<a href='".$_SESSION['beforeAccess']."' class='annulla'>ANNULLA</a>");
