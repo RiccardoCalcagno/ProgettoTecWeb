9653a5cc4dcbdd4a88ca055720576b6b8f0c18df
RiccardoCalcagno <63152853+RiccardoCalcagno@users.noreply.github.com>
2021-01-09 18:38:55 +0100

    d

--- php/banners.php
+++ php/banners.php
@@ -60,12 +60,17 @@
 
     function createPossibleBanner($returnPage) {
         $htmlBanner="";
-        echo "HEY SIAMO IN BANNERS ".$_SESSION['banners'];
-        $_SESSION['banners']=null;
 
         if (isset($_SESSION['banners'])&&($_SESSION['banners'])){
 
             if((strpos($_SESSION['banners'],'lementi_salvati'))&&((isset($_SESSION['stagedReports'])&&($_SESSION['stagedReports']))||(isset($_SESSION['stagedPersonaggi'])&&($_SESSION['stagedPersonaggi'])))){
+
+
+
+                echo "HEY SIAMO IN BANNERS ".$_SESSION['banners']. " con personaggi? ".(isset($_SESSION['stagedPersonaggi'])&&($_SESSION['stagedPersonaggi']));
+
+                
+
                 $htmlBanner="
                     <fieldset id='bannerSalvataggio'>
                     <legend><a xml:lang='en' href='../php/" . $returnPage . "' id='chiusuraBanner'>Close</a></legend>";
