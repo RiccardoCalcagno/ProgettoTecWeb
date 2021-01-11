7c10faba8b95657face5675a987f5311d9cb3656
RiccardoCalcagno <63152853+RiccardoCalcagno@users.noreply.github.com>
2021-01-11 02:27:06 +0100

    d

--- php/check_login.php
+++ php/check_login.php
@@ -1,8 +1,8 @@
 <?php 
+    require_once("report_data.php");
     require_once("DBinterface.php");
     require_once("banners.php");
     require_once("GeneralPurpose.php");
-    require_once("report_data.php");
 
     session_start();
         /*
