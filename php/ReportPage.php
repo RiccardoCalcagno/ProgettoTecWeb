4920bae5c94e136ed097cc392fc460b6b90fdcb7
RiccardoCalcagno <63152853+RiccardoCalcagno@users.noreply.github.com>
2021-01-12 12:26:48 +0100

    d

--- php/ReportPage.php
+++ php/ReportPage.php
@@ -68,6 +68,8 @@ else {
             $commenterPic = array();
             for ($i = 0; $i < count($commentsArray);$i++){
                 $commenterPic[$i] = $dbInterface->getUserPic($commentsArray[$i]->get_author());
+                echo var_dump($commenterPic[$i]);
+                exit();
             }
         }
         
