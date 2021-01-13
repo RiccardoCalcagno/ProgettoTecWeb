14717e7a8750e15a23e613db9242e722e57ca2bb
sertorinicho <72974631+sertorinicho@users.noreply.github.com>
2021-01-13 15:18:13 +0100

    Update TESTING.php

--- php/TESTING.php
+++ php/TESTING.php
@@ -1,9 +1,18 @@
 <?php
-	 require_once("DBinterface.php");
+	require_once("DBinterface.php");
+	require_once("report_data.php");
 
 	 $dbInterface = new DBinterface();
 	 $connection = $dbInterface->openConnection();
-	 echo $dbInterface->getUserId($_SESSION['username']);
+
+	 $lista = array();
+	 array_push($lista,'Grog');
+	 $rep = new ReportData(null, 'aaa', 'bbb', 'ccc', 'ShinigamiVII', false, $lista);
+
+	 echo dbInterface->addReport($rep);
+
 	 $dbInterface->closeConnection();
+
+
 
 ?>
