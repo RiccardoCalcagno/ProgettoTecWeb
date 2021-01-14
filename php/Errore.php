ff95a50a10c9172f998bc1f18252a6de0ebde8bd
Nicola <nicolapavin@gmail.com>
2021-01-14 01:52:19 +0100

    Small fix ErrorPages

--- php/Errore.php
+++ php/Errore.php
@@ -9,14 +9,13 @@ if( session_status() == PHP_SESSION_NONE) {
 else if ( !isset($_SESSION['errorMessage']) ) {
     $_SESSION['errorMessage'] = 'Errore: Nessun Errore :)';
 }
-else {
-    $html = file_get_contents("..". DIRECTORY_SEPARATOR . "html". DIRECTORY_SEPARATOR . "Errore.html");
-    $html = setup_clear($html);
-    $html = addPossibleBanner($html, "Errore.php");
-    $html = str_replace('<TestoErrore />', $_SESSION['errorMessage'], $html);
-    
-    echo $html;
-    
-}
+
+$html = file_get_contents("..". DIRECTORY_SEPARATOR . "html". DIRECTORY_SEPARATOR . "Errore.html");
+$html = setup_clear($html);
+$html = addPossibleBanner($html, "Errore.php");
+$html = str_replace('<TestoErrore />', $_SESSION['errorMessage'], $html);
+
+echo $html;
+
 
 ?>
