0bc74e57eabbf8eae723c99ec4b841df8d0bfe44
Nicola <nicolapavin@gmail.com>
2021-01-14 01:39:01 +0100

    Fixed Errore & 404: PHP & Style

--- php/Errore.php
+++ php/Errore.php
@@ -2,18 +2,21 @@
 require_once("GeneralPurpose.php");
 require_once("banners.php");
 
-// ALTERNATIVA: usare $_SESSION['error'] = 'errorMessage'; e no funzione
-function error($errorMessage) {
 
+if( session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+else if ( !isset($_SESSION['errorMessage']) ) {
+    $_SESSION['errorMessage'] = 'Errore: Nessun Errore :)';
+}
+else {
     $html = file_get_contents("..". DIRECTORY_SEPARATOR . "html". DIRECTORY_SEPARATOR . "Errore.html");
     $html = setup_clear($html);
     $html = addPossibleBanner($html, "Errore.php");
-    $html = str_replace('<TestoErrore />', $errorMessage, $html);
+    $html = str_replace('<TestoErrore />', $_SESSION['errorMessage'], $html);
     
     echo $html;
-
-    exit();
+    
 }
-
 
 ?>
