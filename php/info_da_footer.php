4319acac00f3464b877272c6319607d2ede8e26e
RiccardoCalcagno <63152853+RiccardoCalcagno@users.noreply.github.com>
2021-01-17 00:26:55 +0100

    d

---
+++ php/info_da_footer.php
@@ -0,0 +1,20 @@
+<?php
+require_once("GeneralPurpose.php");
+require_once("banners.php");
+
+
+if( session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+else if ( !isset($_SESSION['errorMessage']) ) {
+    $_SESSION['errorMessage'] = 'Errore: Nessun Errore :)';
+}
+
+$html = file_get_contents("..". DIRECTORY_SEPARATOR . "html". DIRECTORY_SEPARATOR . "info_da_footer.html");
+$html = setup_clear($html);
+$html = addPossibleBanner($html, "info_da_footer.php");
+
+echo $html;
+
+
+?>
