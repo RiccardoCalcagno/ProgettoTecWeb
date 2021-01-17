d1f99cad015ceeb45f6dc46435d9627f4170f7dc
Nicola <nicolapavin@gmail.com>
2021-01-17 04:21:41 +0100

    FOOOOOTER

--- js/scripts.js
+++ js/scripts.js
@@ -201,16 +201,7 @@ function CharSheet_JS_ON() {
 
 function footer_JS_ON() { // IF JS ON -> interactive footer
 
-    document.getElementById("tornaSuFooter").classList.remove("hidden");
-
-    var footerList = document.getElementById("footerContent"); 
-    footerList.className = "interactive";   // Switch class
-
-    var buttons = footerList.querySelectorAll("button");
-    for(i = 0; i < 4; i++) {
-
-        buttons[i].removeAttribute("disabled");
-    }
+    document.getElementById("globalFooter").className = "interactive";
 }
 
 window.addEventListener('load', function() { footer_JS_ON();}, false) ;
