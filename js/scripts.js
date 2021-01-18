400aa47a893318ee07f0ff1b86d9f994be82d579
RiccardoCalcagno <63152853+RiccardoCalcagno@users.noreply.github.com>
2021-01-18 01:37:57 +0100

    d

--- js/scripts.js
+++ js/scripts.js
@@ -295,6 +295,7 @@ function mostraErrore(input, inputArray) {
 
     var elemento = document.createElement("strong");
     elemento.className = "text-errore";
+    elemento.setAttribute("tabindex","10");
     //elemento.appendChild(document.createTextNode(testArray[input.id][1])); e' piu' giusto?
     elemento.innerHTML = inputArray[1];    // Per usare tags all'interno
     
