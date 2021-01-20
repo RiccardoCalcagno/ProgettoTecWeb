9104d746bf924e7d397f397bba65f5f1e781a766
RiccardoCalcagno <63152853+RiccardoCalcagno@users.noreply.github.com>
2021-01-20 19:43:05 +0100

    d

--- index.php
+++ index.php
@@ -7,6 +7,14 @@ $html = file_get_contents("html" . DIRECTORY_SEPARATOR . "Home.html");
 $html = setup_clear($html);
 $html = str_replace('href="../php/InfoFooter.php"', 'href="php/InfoFooter.php"', $html);    // fix footer rel. path
 
+if(isset($_GET["Hamburger"])){
+
+    if($_GET["Hamburger"]=="yes"){
+        $html = str_replace("<ul id=\"menu\"", "<ul id=\"menu\" style=\"display:block;\"", $html);
+        $html = str_replace("../index.php?Hamburger=yes", "../index.php?Hamburger=no", $html);
+    }
+}
+
 $html = addPossibleBanner($html, "index.php");
 
 echo $html;
