d7cdd0730405cfc9a486ed0610757a776cea0b83
RiccardoCalcagno <63152853+RiccardoCalcagno@users.noreply.github.com>
2021-01-24 19:01:23 +0100

    d

--- php/CreazioneReportPage.php
+++ php/CreazioneReportPage.php
@@ -118,8 +118,6 @@
                     $rep = new ReportData($id_report, $titolo, $sottotitolo, $contenuto, null, $condividi, $_SESSION['listaGiocatori']);
                     array_push($_SESSION['stagedReports'], $rep);
                     $_SESSION['banners']= "salvataggio_pendente";
-                    header("Location: CreazioneReportPage.php#bannerID");
-                    exit();
                 }
                 }else{
                 $message = '<div id="errori" class="" tabindex="10" aria-label="sono stati riscontrati alcuni errori. ti trovi all\' inizio della lista di input"><ul>'; // TO FIX
